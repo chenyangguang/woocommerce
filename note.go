51eabf7f9867262b5c79221d4c6c71d3eada3928
chenyangguang <vim.09071@gmail.com>
2021-01-25 15:22:01 +0800

    + add orderNote

--- note.go
+++ note.go
@@ -1,11 +1,17 @@
 package woocommerce
 
+import "fmt"
+
+const (
+	orderNoteBasePath = "orders"
+)
+
 // OrderNoteService operate Woo-Commerce Order note, eg: create, view, and delete individual order notes.
 // https://woocommerce.github.io/woocommerce-rest-api-docs/#order-notes
 type OrderNoteService interface {
-	Create()
-	Get()
-	List()
+	Create(text string) (*OrderNote, error)
+	Get(orderId int64, noteId int64) (*OrderNote, error)
+	List(orderId int64, options interface{})
 	Delete()
 }
 
@@ -26,7 +32,35 @@ type OrderNoteServiceOp struct {
 	client *Client
 }
 
-func (n *OrderNoteServiceOp) Create() {}
-func (n *OrderNoteServiceOp) Get()    {}
-func (n *OrderNoteServiceOp) List()   {}
-func (n *OrderNoteServiceOp) Delete() {}
+func (n *OrderNoteServiceOp) Create(orderId int64, text string) (*OrderNote, error) {
+	path := fmt.Sprintf("%s/%d/notes", orderNoteBasePath, orderId)
+	resource := new(OrderNote)
+	insertOrderNote := OrderNote{
+		Note: text,
+	}
+	err := n.client.Post(path, insertOrderNote, resource)
+	return resource, err
+}
+
+func (n *OrderNoteServiceOp) Get(orderId int64, noteId int64) (*OrderNote, error) {
+	path := fmt.Sprintf("%s/%d/notes/%d", orderNoteBasePath, orderId, noteId)
+	resource := new(OrderNote)
+
+	err := n.client.Get(path, resource, nil)
+	return resource, err
+}
+
+func (n *OrderNoteServiceOp) List(orderId int64, optoions interface{}) ([]Order, error) {
+	path := fmt.Sprintf("%s/%d/notes", orderNoteBasePath, orderId)
+	resource := new(OrderNote)
+
+	err := n.client.Get(path, resource, options)
+	return resource, err
+}
+
+func (n *OrderNoteServiceOp) Delete(orderId int64, noteId int64, options interface{}) (*OrderNote, error) {
+	path := fmt.Sprintf("%s/%d/notes/%d", orderNoteBasePath, orderId, noteId)
+	resource := new(OrderNote)
+	err := n.client.Delete(path, options, &resource)
+	return resource, err
+}
